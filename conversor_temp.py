a279cbfa5179dbbaca1b70741679b8596390cb81
Karin Centenaro <98807488+KarinTech@users.noreply.github.com>
2022-10-08 10:22:14 -0300

    Add files via upload

---
+++ conversor_temp.py
@@ -0,0 +1,9 @@
+
+# Online Python - IDE, Editor, Compiler, Interpreter
+kelvin=int(input("Digite la temperatura en Kelvin "))
+celsius=((kelvin) -273)
+fahrenheit=int(input("Digite la temperatura en Fahrenheit"))
+celsius2=((fahrenheit)-32*5/9)
+print("La temperatura en celsius es ",(celsius))
+print("##########################")
+print("La temperatura en Celsius es ", (celsius2))
